d5a03321d907670aecf404d8a8ea68391157d201
Harrison Walters <hwalters@uvic.ca>
2017-03-27 13:13:12 -0700

    update2

--- Main.py
+++ Main.py
@@ -8,24 +8,9 @@ import numpy
 def main():
 
 	# initalize PyAudio
+	WIDTH = 2
 	p = pyaudio.PyAudio()
-	stream = p.open(format=p.get_format_from_width(WIDTH),
-                channels=1,
-                rate=11025,
-                input=True,
-                output=False,
-                frames_per_buffer=(1024*2)
+	stream = p.open(format=p.get_format_from_width(WIDTH),channels=1,rate=11025,input=True,output=False,frames_per_buffer=(1024*2))
 
-print("* recording")
-	
-	"""if (len(sys.argv) <= 1):
-		print("\nTo use, type 'py Main.py <command>'.\nCommands include -mine or -train or -test")
-	elif (sys.argv[1] == "-mine"):
-		Scraper.StartCollectingData()
-	elif (sys.argv[1] == "-train"):
-		SVM.TrainOnData()
-	else:
-		print("unknown mode")"""
-	
 if __name__ == "__main__":
     main()
