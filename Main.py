31f55aa9f04dfdccee1758525f3b423df8e96de7
Harrison Walters <hwalters@uvic.ca>
2017-03-27 13:17:51 -0700

    update

--- Main.py
+++ Main.py
@@ -10,7 +10,13 @@ def main():
 	# initalize PyAudio
 	WIDTH = 2
 	p = pyaudio.PyAudio()
-	stream = p.open(format=p.get_format_from_width(WIDTH),channels=1,rate=11025,input=True,output=False,frames_per_buffer=(1024*2))
-
+	audio_stream = p.open(format=p.get_format_from_width(WIDTH),channels=1,rate=11025,input=True,output=False,frames_per_buffer=(1024*2))
+	print("*recording")
+	
+	# clean up
+	audio_stream.stop_stream()
+	audio_stream.close()
+	p.terminate()
+	
 if __name__ == "__main__":
     main()
