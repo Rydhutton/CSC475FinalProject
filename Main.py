df5a52799b4b348e3b88d2b778c4b81d94837175
Harrison Walters <hwalters@uvic.ca>
2017-03-27 13:09:02 -0700

    update

--- Main.py
+++ Main.py
@@ -2,9 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import pyaudio
+import numpy
 
 def main():
-	poop = 3
+
+	# initalize PyAudio
+	p = pyaudio.PyAudio()
+	stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=1,
+                rate=11025,
+                input=True,
+                output=False,
+                frames_per_buffer=(1024*2)
+
+print("* recording")
+	
 	"""if (len(sys.argv) <= 1):
 		print("\nTo use, type 'py Main.py <command>'.\nCommands include -mine or -train or -test")
 	elif (sys.argv[1] == "-mine"):
