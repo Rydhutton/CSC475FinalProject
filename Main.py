0031d1c5daf57488616140c6f58bec243cd89b8f
Harrison Walters <hwalters@uvic.ca>
2017-03-29 16:20:45 -0700

    update

--- Main.py
+++ Main.py
@@ -7,36 +7,46 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-CHUNK = 1024
-DTYPE = np.int16
-wf = wave.open('w.wav', 'rb')
+def main():
+	process()
 
-# begin audio stream
-p = pyaudio.PyAudio()
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),channels=2,rate=wf.getframerate(),output=True)
+def process():
+	CHUNK = 1024
+	DTYPE = np.int16
+	wf = wave.open('w.wav', 'rb')
 
-# read data
-data = wf.readframes(CHUNK)
+	# begin audio stream
+	p = pyaudio.PyAudio()
+	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),channels=2,rate=wf.getframerate(),output=True)
 
-while len(data) > 0:
-	# read from file, convert to usable form
-	audio_data = np.fromstring(data, dtype=DTYPE)
-		
-	stereo_data = np.zeros([CHUNK,2])
-	for i in range(int(len(audio_data)/2)):
-		x = i*2
-		stereo_data[i,0] = -audio_data[x]
-		stereo_data[i,1] = audio_data[x]
-	
-	# write audio data to output
-	out_data = np.array(stereo_data, dtype=DTYPE)
-	string_audio_data = out_data.tostring()
-	stream.write(string_audio_data, CHUNK)
-	
-	# fetch new audio data
+	# read data
 	data = wf.readframes(CHUNK)
 
-# cleanup
-stream.stop_stream()
-stream.close()
-p.terminate()
+	while len(data) > 0:
+		# read from file, convert to usable form
+		audio_data = np.fromstring(data, dtype=DTYPE)
+			
+		# split audio data into two channels
+		# calculate anti-signal
+		stereo_data = np.zeros([CHUNK,2])
+		for i in range(int(len(audio_data)/2)):
+			x = i*2
+			stereo_data[i,0] = -audio_data[x]
+			stereo_data[i,1] = audio_data[x]
+		
+		# write audio data to output
+		out_data = np.array(stereo_data, dtype=DTYPE)
+		string_audio_data = out_data.tostring()
+		stream.write(string_audio_data, CHUNK)
+		
+		# fetch new audio data
+		data = wf.readframes(CHUNK)
+
+	# cleanup
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
+if __name__ == "__main__":
+    main()
+
