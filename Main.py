2c6b9fae4d68df0efabab0abf69d9deb4789fb0a
Harrison Walters <hwalters@uvic.ca>
2017-03-29 17:03:54 -0700

    update

--- Main.py
+++ Main.py
@@ -6,15 +6,19 @@ import wave
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
+CHUNK = 1024
+DTYPE = np.int16
 
 def main():
 	# determine launch context
-	if (sys.argv[1] == 'di'):
+	if (sys.argv[1] == 'sin'):
+		sin_wave_ex()
+	elif (sys.argv[1] == 'di'):
 		destructive_interference_demo()
 		
 def destructive_interference_demo():
-	CHUNK = 1024
-	DTYPE = np.int16
 	wf = wave.open(sys.argv[2], 'rb')
 
 	# begin audio stream
@@ -47,6 +51,31 @@ def destructive_interference_demo():
 	stream.close()
 	p.terminate()
 
+def sin_wave_ex():
+
+	# begin audio stream
+	p = pyaudio.PyAudio()
+	stream = p.open(format=p.get_format_from_width(2),channels=2,rate=11025,output=True)
+	
+	i  = 0.0
+	while True:
+	
+		stereo_data = np.zeros([CHUNK,2])
+		for n in range(CHUNK):
+			stereo_data[n,0] = math.sin(i/10.0)*30000 #left speaker
+			stereo_data[n,1] = math.sin(i/10.0)*30000 #right speaker
+			i+=1.0
+	
+		# write audio data to output
+		out_data = np.array(stereo_data, dtype=DTYPE)
+		string_audio_data = out_data.tostring()
+		stream.write(string_audio_data, CHUNK)
+	
+	# cleanup
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+	
 if __name__ == "__main__":
     main()
 
