1da914336b544b4609114ede9674a98555f81b3e
Harrison Walters <hwalters@uvic.ca>
2017-04-05 22:31:30 -0700

    update

--- Main.py
+++ Main.py
@@ -9,8 +9,8 @@ import numpy as np
 import math
 from scipy import *
 
-#CHUNK = 8192
-CHUNK = 16384
+CHUNK = 8192
+#CHUNK = 16384
 MAX = 32768.0
 DTYPE = np.int16
 
@@ -22,70 +22,12 @@ def main():
 	# "python Main.py sin" = test sine wave output
 	
 	if (sys.argv[1] == 'sin'): 
-		sin_wave_ex()
+		debug_sine()
 	elif (sys.argv[1] == 'di'): 
 		destructive_interference_demo()
 	else:
 		calculate() 
 		
-def destructive_interference_demo():
-	wf = wave.open(sys.argv[2], 'rb')
-
-	# begin audio stream
-	p = pyaudio.PyAudio()
-	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),channels=2,rate=wf.getframerate(),output=True)
-
-	# read data from wav file
-	data = wf.readframes(CHUNK)
-	while len(data) > 0:
-		# read from file, convert to usable form
-		audio_data = np.fromstring(data, dtype=DTYPE)
-			
-		# split audio data into two channels & calculate anti-signal
-		stereo_data = np.zeros([CHUNK,2])
-		for i in range(int(len(audio_data)/2)):
-			x = i*2
-			stereo_data[i,0] = audio_data[x] #left speaker
-			stereo_data[i,1] = -audio_data[x] #right speaker
-		
-		# write audio data to output
-		out_data = np.array(stereo_data, dtype=DTYPE)
-		string_audio_data = out_data.tostring()
-		stream.write(string_audio_data, CHUNK)
-		
-		# fetch new audio data
-		data = wf.readframes(CHUNK)
-
-	# cleanup
-	stream.stop_stream()
-	stream.close()
-	p.terminate()
-	
-def sin_wave_ex():
-
-	# begin audio stream
-	p = pyaudio.PyAudio()
-	stream = p.open(format=p.get_format_from_width(2),channels=2,rate=11025,output=True)
-	
-	# write sine wave data
-	i  = 0.0
-	while True:
-		stereo_data = np.zeros([CHUNK,2])
-		for n in range(CHUNK):
-			stereo_data[n,0] = math.sin(i/10.0)*30000 #left speaker
-			stereo_data[n,1] = math.sin(i/10.0)*30000 #right speaker
-			i+=1.0
-	
-		# write audio data to output
-		out_data = np.array(stereo_data, dtype=DTYPE)
-		string_audio_data = out_data.tostring()
-		stream.write(string_audio_data, CHUNK)
-	
-	# cleanup
-	stream.stop_stream()
-	stream.close()
-	p.terminate()
-
 def calculate():
 	wf = wave.open(sys.argv[1], 'rb')
 
@@ -111,7 +53,7 @@ def calculate():
 	print (b)
 	
 	# config
-	n_averaging_set = 1
+	n_averaging_set = 2
 	delay = 1
 	
 	# vars
@@ -120,7 +62,7 @@ def calculate():
 	delayed_data = []
 	averaging_set = []
 	data = wf.readframes(CHUNK)
-	T = delay
+	T = delay+1
 	
 	# initialize
 	for i in range(delay):
@@ -180,8 +122,65 @@ def calculate():
 		if (sys.argv[2] == "plot"):
 			plt.figure(figsize=(15,4))
 			plt.plot(plot_data, 'b', plot_sum, 'r')
-			plt.show()
+			plt.show()		
+		
+def destructive_interference_demo():
+	wf = wave.open(sys.argv[2], 'rb')
 
+	# begin audio stream
+	p = pyaudio.PyAudio()
+	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),channels=2,rate=wf.getframerate(),output=True)
+
+	# read data from wav file
+	data = wf.readframes(CHUNK)
+	while len(data) > 0:
+		# read from file, convert to usable form
+		audio_data = np.fromstring(data, dtype=DTYPE)
+			
+		# split audio data into two channels & calculate anti-signal
+		stereo_data = np.zeros([CHUNK,2])
+		for i in range(int(len(audio_data)/2)):
+			x = i*2
+			stereo_data[i,0] = audio_data[x] #left speaker
+			stereo_data[i,1] = -audio_data[x] #right speaker
+		
+		# write audio data to output
+		out_data = np.array(stereo_data, dtype=DTYPE)
+		string_audio_data = out_data.tostring()
+		stream.write(string_audio_data, CHUNK)
+		
+		# fetch new audio data
+		data = wf.readframes(CHUNK)
+
+	# cleanup
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+	
+def debug_sine():
+
+	# begin audio stream
+	p = pyaudio.PyAudio()
+	stream = p.open(format=p.get_format_from_width(2),channels=2,rate=11025,output=True)
+	
+	# write sine wave data
+	i  = 0.0
+	while True:
+		stereo_data = np.zeros([CHUNK,2])
+		for n in range(CHUNK):
+			stereo_data[n,0] = math.sin(i/10.0)*30000 #left speaker
+			stereo_data[n,1] = math.sin(i/10.0)*30000 #right speaker
+			i+=1.0
+	
+		# write audio data to output
+		out_data = np.array(stereo_data, dtype=DTYPE)
+		string_audio_data = out_data.tostring()
+		stream.write(string_audio_data, CHUNK)
+	
+	# cleanup
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
 	
 if __name__ == "__main__":
     main()
